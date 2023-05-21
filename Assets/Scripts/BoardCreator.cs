4b7785e4ae3d2fc3774a1f51f3407f2aeb72b81e
iidal <iida.lintuaho@gmail.com>
2023-05-21 18:40:51 +0300

    initial

---
+++ Assets/Scripts/BoardCreator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardCreator : MonoBehaviour
+{
+        //this script will receive a class of info needed for creating the board
+        // first it will need the size of the board
+    public GameObject m_boardbox;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PopulateBoard(int width, int height){
+
+    }
+}
